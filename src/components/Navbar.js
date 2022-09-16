746e66af689150054df762c4a8cfdc182c7d0780
Yachao Fan <61946174+ycfan23@users.noreply.github.com>
2022-09-16 12:57:43 -0400

    added img to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import './Navbar.css'
+import './Navbar.css';
+import Logo from '../assets/sailboat_icon.png';
 // import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <>
       <nav className="navbar">
+        <img src={Logo} alt="Logo" />
         <a href="/" className="site-title">Sushi Boat</a>
         <ul>
-          <li><a href="/menu">Menu</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/location">Hours & Location</a></li>
-          <li><a href="/contact">Contact Us</a></li>
+          <li><a href="/menu" className="menu-list">Menu</a></li>
+          <li><a href="/about" className="menu-list">About</a></li>
+          <li><a href="/location" className="menu-list">Hours & Location</a></li>
+          <li><a href="/contact" className="menu-list">Contact Us</a></li>
         </ul>
       </nav>
     </>
